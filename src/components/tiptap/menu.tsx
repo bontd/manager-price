d1016842c051cf51c3a32a7aaa5c499fe00d8c45
bontd94 <bontd@mumesoft.vn>
2025-07-25 17:51:16 +0700

    update style button tiptap

--- src/components/tiptap/menu.tsx
+++ src/components/tiptap/menu.tsx
@@ -43,19 +43,19 @@ const TiptapMenu = ({ editor }: { editor: Editor | null }) => {
     });
 
     const headingItems = [
-        { key: '1', label: (<Button className="btn-custom" onClick={() => editor?.chain().focus().toggleHeading({ level: 1 as Level }).run()}>H1</Button>), level: 1 },
-        { key: '2', label: (<Button className="btn-custom" onClick={() => editor?.chain().focus().toggleHeading({ level: 2 as Level }).run()}>H2</Button>), level: 2 },
-        { key: '3', label: (<Button className="btn-custom" onClick={() => editor?.chain().focus().toggleHeading({ level: 3 as Level }).run()}>H3</Button>), level: 3 },
-        { key: '4', label: (<Button className="btn-custom" onClick={() => editor?.chain().focus().toggleHeading({ level: 4 as Level }).run()}>H4</Button>), level: 4 },
-        { key: '5', label: (<Button className="btn-custom" onClick={() => editor?.chain().focus().toggleHeading({ level: 5 as Level }).run()}>H5</Button>), level: 5 },
-        { key: '6', label: (<Button className="btn-custom" onClick={() => editor?.chain().focus().toggleHeading({ level: 6 as Level }).run()}>H6</Button>), level: 6 },
+        { key: '1', label: (<Button onClick={() => editor?.chain().focus().toggleHeading({ level: 1 as Level }).run()}>H1</Button>), level: 1 },
+        { key: '2', label: (<Button onClick={() => editor?.chain().focus().toggleHeading({ level: 2 as Level }).run()}>H2</Button>), level: 2 },
+        { key: '3', label: (<Button onClick={() => editor?.chain().focus().toggleHeading({ level: 3 as Level }).run()}>H3</Button>), level: 3 },
+        { key: '4', label: (<Button onClick={() => editor?.chain().focus().toggleHeading({ level: 4 as Level }).run()}>H4</Button>), level: 4 },
+        { key: '5', label: (<Button onClick={() => editor?.chain().focus().toggleHeading({ level: 5 as Level }).run()}>H5</Button>), level: 5 },
+        { key: '6', label: (<Button onClick={() => editor?.chain().focus().toggleHeading({ level: 6 as Level }).run()}>H6</Button>), level: 6 },
     ];
 
     const listItems = [
         {
             key: '1',
             label: (
-                <Button className="btn-custom" onClick={() => editor.chain().focus().toggleBulletList().run()} disabled={!editor.can().toggleBulletList()}>
+                <Button onClick={() => editor.chain().focus().toggleBulletList().run()} disabled={!editor.can().toggleBulletList()}>
                     <img src={IcoBluletList} alt="Bullet List" />
                 </Button>
             ),
@@ -63,7 +63,7 @@ const TiptapMenu = ({ editor }: { editor: Editor | null }) => {
         {
             key: '2',
             label: (
-                <Button className="btn-custom" onClick={() => editor.chain().focus().toggleOrderedList().run()} disabled={!editor.can().toggleOrderedList()}>
+                <Button onClick={() => editor.chain().focus().toggleOrderedList().run()} disabled={!editor.can().toggleOrderedList()}>
                     <img src={IcoOrderedList} alt="Ordered List" />
                 </Button>
             ),
@@ -71,7 +71,7 @@ const TiptapMenu = ({ editor }: { editor: Editor | null }) => {
         {
             key: '3',
             label: (
-                <Button className="btn-custom" onClick={() => editor.chain().focus().toggleTaskList().run()}>
+                <Button onClick={() => editor.chain().focus().toggleTaskList().run()}>
                     <img src={IcoTaskList} alt="Task List" />
                 </Button>
             ),
@@ -79,17 +79,17 @@ const TiptapMenu = ({ editor }: { editor: Editor | null }) => {
     ];
 
     return (
-        <div className="group-btn flex gap-2">
+        <div className="group-btn flex gap-2 border border-solid border-[#E5E7EB] border-b-0 rounded-t-[5px] bg-white p-2">
             <Button onClick={() => editor.chain().focus().undo().run()} disabled={!editorState.canUndo}>
                 <img src={IcoUndo} alt="Undo" />
             </Button>
             <Button onClick={() => editor.chain().focus().redo().run()} disabled={!editorState.canRedo}>
                 <img src={IcoRedo} alt="Redo" />
             </Button>
-            <Dropdown menu={{ items: headingItems }} trigger={['click']}>
+            <Dropdown menu={{ items: headingItems }} trigger={['click']} className="c-dropdown">
                 <div className="flex align-center text-[16px] px-[10px] py-[3px]">H <DownOutlined className="text-[8px] ml-[5px]" /></div>
             </Dropdown>
-            <Dropdown menu={{ items: listItems }} trigger={['click']}>
+            <Dropdown menu={{ items: listItems }} trigger={['click']} className="c-dropdown">
                 <img src={IcoBluletList} alt="" />
             </Dropdown>
         </div>
